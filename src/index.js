ec100ff3ef32640bf3e8938757c87b23bc9f2a66
jinyoung4478 <skswls0@daum.net>
2022-04-03 01:38:24 +0900

    1.01 Connect Firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import firebase from "./firebase";
+
+console.log(firebase);
+
+const rootElement = document.getElementById('root');
+const ReactDOM = createRoot(rootElement);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
 );
