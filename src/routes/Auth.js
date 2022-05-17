579f866bf5c2f93a4f151fc19f669746ccc10e2f
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-05-17 11:52:35 +0900

    Update

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <div>
+    <form onSubmit={onSubmit}>
+        <input type="text" name="email" placeholder="Email" value={email} onChange={onChange} required />
+        <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
+        <input type="submit" value="Log In" />
+    </form>
+    <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+    </div>
+</div>
+    )
+}
 export default Auth;
