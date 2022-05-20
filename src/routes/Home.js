3f367b883752878f33147b029e6c7590f95303fa
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-05-20 14:40:02 +0900

    Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,56 @@
-import React from "react";
+import { dbService } from "fbase";
+import { addDoc, collection, getDocs, serverTimestamp, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async() => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await addDoc(collection(dbService, "nweets"), {
+                nweet,
+                createdAt: serverTimestamp(),
+            })
+        } catch (error) {
+            console.log("Error adding document: ", error);
+        }
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { target:{value}} = event;
+        setNweet(value);
+    };
+    console.log(nweets);
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 export default Home;
