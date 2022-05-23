47592f7e6e1c71b34cfbd22f152dd169ead86183
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-05-23 18:34:01 +0900

    Update Nweet.js

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,23 @@
 import React from "react";
 
-const Nweet = ({nweetObj, isOwner}) => (
-    <div>
-        <h4>{nweetObj.text}</h4>
-        {isOwner && (
-            <>
-            <button>Delete Nweet</button>
-            <button>Edit Nweet</button>
-            </>
-        )}
-        
-    </div>
-)
+const Nweet = ({nweetObj, isOwner}) => {
+    const onDeleteClick = () => {
+        const ok = window.confirm("Are you sure you wnat to delete this nweet?");
+        console.log(ok);
+    }
+    return (
+        <div>
+            <h4>{nweetObj.text}</h4>
+            {isOwner && (
+                <>
+                <button onClick={onDeleteClick}>Delete Nweet</button>
+                <button>Edit Nweet</button>
+                </>
+            )}
+            
+        </div>
+    )
+}
+
 
 export default Nweet;
