642d1cb8b431ac46f53121338f428d1671244ba9
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-05-29 17:53:16 +0900

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
     {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing...."}
-    <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    {/* <footer>&copy; Nwitter {new Date().getFullYear()}</footer> */}
     </>
   
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -35,11 +35,21 @@ const Home = ({ userObj }) => {
         const { target:{value}} = event;
         setNweet(value);
     };
+    const onFileChange = (event) => {
+        const {target:{files},
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+        console.log(finishedEvent);
+    }
+    reader.readAsDataURL(theFile);
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
-                <input type="file" accept="image/*" />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
             </form>
             <div>
