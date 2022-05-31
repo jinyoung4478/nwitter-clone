9108222b34c71be93548898d2ec07f335f62a467
jinyoung4478 <102174146+jinyoung4478@users.noreply.github.com>
2022-05-31 18:54:19 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import { dbService } from "fbase";
 import { addDoc, collection, serverTimestamp, query, onSnapshot, orderBy } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         const q = query(collection(dbService, "nweets"),
         orderBy("createdAt")
@@ -41,16 +42,30 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-        console.log(finishedEvent);
+        const {
+            currentTarget: {result},
+        } = finishedEvent;
+        setAttachment(result);
     }
     reader.readAsDataURL(theFile);
     }
+    const fileInput = useRef();
+    const onClearAttachmentClick = () => {
+        setAttachment(null);
+        fileInput.current.value = "";
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
-                <input type="file" accept="image/*" onChange={onFileChange} />
+                <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
                 <input type="submit" value="Nweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" alt="img" />
+                        <button onClick={onClearAttachmentClick}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {nweets.map((nweet) => (
